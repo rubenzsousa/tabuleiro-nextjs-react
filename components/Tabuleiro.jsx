fdc121b12431dc8daa79990ae313fd8a4a45e6e9
Rubens Sousa <rubensfk18@gmail.com>
2021-09-06 14:40:10 -0300

    first commit

---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "./Linha"
+
+export default function Tabuleiro(props){
+    return(
+        <div>
+            <Linha preta/>
+            <Linha />
+            <Linha preta/>
+            <Linha />
+            <Linha preta/>
+            <Linha />
+            <Linha preta/>
+            <Linha />
+        </div>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+
+import Linha from '../components/Linha'
+import Subdivisao from '../components/Subdivisao'
+import Tabuleiro from '../components/Tabuleiro'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Tabuleiro/>
+    </div>
+  )
+}
